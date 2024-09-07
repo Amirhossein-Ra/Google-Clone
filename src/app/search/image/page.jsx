1bd35c42e976865afb46c3e09e3a182de6924532
Amirhossein-Ra <000amir111rahzany@gmail.com>
2024-09-07 21:04:32 +0330

    adding layout

---
+++ src/app/search/image/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function imageSearchPage() {
+  return <div>imageSearchPage</div>;
+}
---
+++ src/app/search/web/page.jsx
@@ -0,0 +1,3 @@
+export default function webSearchage() {
+  return <div>webSearch</div>;
+}
